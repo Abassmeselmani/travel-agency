2574d4986af231624f21a64ee8e5154f7dabf79c
Abass Meselmani <abassmeselmani905@gmail.com>
2025-05-18 23:08:44 +0300

    Added hover effect and updated hover-text styling

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,31 +2,30 @@ import React, { useRef, useEffect, useState } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
+// Font Awesome
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlane } from "@fortawesome/free-solid-svg-icons";
+
+// Image Imports
 import image from "../page1images/image.png";
 import image2 from "../page1images/image2.png";
 import image3 from "../page1images/image3.png";
 import image4 from "../page1images/offer-bg-2.webp";
 import image5 from "../page1images/tst-1.webp";
-
 import image6 from "../page1images/tst-2.webp";
 import image7 from "../page1images/tst-3.webp";
 import image8 from "../page1images/tst-4.webp";
 import image9 from "../page1images/about-image-1.webp";
-
 import image10 from "../page1images/about-element-3.webp";
 import image11 from "../page1images/about-element-1.webp";
+import image12 from "../page1images/Discover-image-1.webp";
+import image13 from "../page1images/Discover-image-2.webp";
+import image14 from "../page1images/Discover-image-3.webp";
+import image15 from "../page1images/Discover-image-4.webp";
 
-
-
-
-
-
+// Data and Styles
 import { dest } from "../destination";
 import "./home.css";
-
-// Font Awesome
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlane } from "@fortawesome/free-solid-svg-icons";
 
 function Home() {
   const sliderRef = useRef(null);
@@ -52,7 +51,7 @@ function Home() {
   };
 
   const resetAOS = () => {
-    setTitleKey((prev) => prev + 1);
+    setTitleKey(prev => prev + 1);
     AOS.refresh();
   };
 
@@ -79,6 +78,7 @@ function Home() {
 
   return (
     <>
+      {/* Intro Section */}
       <div className="Intropage">
         <button className="arrow left" onClick={handlePrev}>
           &#10094;
@@ -100,6 +100,7 @@ function Home() {
         </h1>
       </div>
 
+      {/* Destination Section */}
       <div className="destination">
         <h1 className="destination-title" data-aos="fade-left">
           <FontAwesomeIcon icon={faPlane} className="airplane-icon" />
@@ -109,8 +110,7 @@ function Home() {
 
         <h1 data-aos="fade-right">Top Destinations</h1>
         <p className="destination-descript" data-aos="fade-bottom">
-          Explore our top destinations voted by more than 100,000+ customers
-          around the world
+          Explore our top destinations voted by more than 100,000+ customers around the world
         </p>
       </div>
 
@@ -121,11 +121,7 @@ function Home() {
       >
         {[...dest, ...dest, ...dest].map((item, index) => (
           <div key={index} className="destination-card">
-            <img
-              src={item.imageURL}
-              alt={item.name}
-              className="destination-image"
-            />
+            <img src={item.imageURL} alt={item.name} className="destination-image" />
             <span className="destination-price">{item.price}</span>
             <div className="destination-info">
               <h3 className="destination-name">{item.name}</h3>
@@ -136,43 +132,88 @@ function Home() {
         ))}
       </div>
 
-      {/* Moved outside destination-list for visibility */}
+      {/* Company Info Section */}
       <div className="infocompany" data-aos="fade-up">
         <h1 className="infocompany-title" data-aos="fade-right">
           <FontAwesomeIcon icon={faPlane} className="airplane-icon" />
           &nbsp;About our Company&nbsp;
           <FontAwesomeIcon icon={faPlane} className="airplane-icon flipped" />
         </h1>
-        <h1 className="infocompany-descript" data-aos="fade-right">Experience Our World With our Company</h1>
-        <p className="infocomapany-paragraph" data-aos="fade-right">Embark on a trasformative journey as our company invites you to experience the 
-          world creating unforgettable memories through exceptional travel adventures </p>
-          <div className="infocompany-part2" data-aos="fade-right">
-            <img className="infocompany-part2-img" src={image4}/>
-            <ul className="infocompany-part2-list" data-aos="fade-right">
-              <li>Perfect Detailing</li>
-              <li>Completed Certification</li>
-              <li>Save your budget</li>
-              <li>Free Consultation</li>
-            </ul>
 
+        <h1 className="infocompany-descript" data-aos="fade-right">
+          Experience Our World With our Company
+        </h1>
+        <p className="infocomapany-paragraph" data-aos="fade-right">
+          Embark on a transformative journey as our company invites you to experience the
+          world creating unforgettable memories through exceptional travel adventures
+        </p>
 
+        <div className="infocompany-part2" data-aos="fade-right">
+          <img className="infocompany-part2-img" src={image4} alt="Offer" />
+          <ul className="infocompany-part2-list">
+            <li>Perfect Detailing</li>
+            <li>Completed Certification</li>
+            <li>Save your budget</li>
+            <li>Free Consultation</li>
+          </ul>
+        </div>
 
-          </div>
-          <div className="infocompany-part3" data-aos="fade-right">
+        <div className="infocompany-part3" data-aos="fade-right">
           <button className="infocompany-part2-btn">Learn More</button>
           <div className="infocompany-part3-img-p">
-            <img src={image5}/>
-            <img src={image6}/>
-            <img src={image7}/>
-            <img src={image8}/>
+            <img src={image5} alt="Client 1" />
+            <img src={image6} alt="Client 2" />
+            <img src={image7} alt="Client 3" />
+            <img src={image8} alt="Client 4" />
             <p>5.2+ satisfied clients</p>
-
           </div>
+        </div>
 
+        <img className="infocompany-main" src={image9} alt="Main" data-aos="fade-left" />
+        <img className="infocompany-main2" src={image10} alt="Decor 1" data-aos="fade-left" />
+        <img className="infocompany-main3" src={image11} alt="Decor 2" data-aos="fade-left" />
+      </div>
+
+      {/* Discover Section */}
+      <div className="discoverinfo">
+        <h1 className="destination-title" data-aos="fade-left">
+          <FontAwesomeIcon icon={faPlane} className="airplane-icon" />
+          &nbsp;Travel Destination&nbsp;
+          <FontAwesomeIcon icon={faPlane} className="airplane-icon flipped" />
+        </h1>
+
+        <h1 data-aos="fade-left" className="discoverinfo-title">Discover the touch of Nature</h1>
+        <p data-aos="fade-right" className="discoverinfo-discript">
+          Our clients rave about our exceptional seamless experiences that exceeded their expectations
+        </p>
+
+        <div data-aos="fade-left" className="discoverinfo-img">
+          <div className="hover-image">
+            <img src={image12} alt="Mountains" />
+            
+            <div className="hover-text">Beautiful tropical beach sea with umbrella
+              chair around suimming  pool
+            </div>
           </div>
-          <img className="infocompany-main" data-aos="fade-left" src={image9}/>
-          <img className="infocompany-main2" data-aos="fade-left" src={image10}/>
-          <img className="infocompany-main3" data-aos="fade-left" src={image11}/>
+          <div className="hover-image">
+            <img src={image13} alt="Nature Walk" />
+            <div className="hover-text">Beautiful tropical beach sea with umbrella
+              chair around suimming  pool
+            </div>
+          </div>
+          <div className="hover-image">
+            <img src={image14} alt="Wildlife" />
+            <div className="hover-text">Beautiful tropical beach sea with umbrella
+              chair around suimming  pool
+            </div>
+          </div>
+          <div className="hover-image">
+            <img src={image15} alt="Adventure" />
+            <div className="hover-text">Beautiful tropical beach sea with umbrella
+              chair around suimming  pool
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
